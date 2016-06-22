4ee65dfcb9d28eba3bb8f552006deea77431d716
Jon Barlow <orviwan@gmail.com>
2016-06-22 14:25:08 -0700

    missing file

---
+++ src/main.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <pebble.h>
+
+#define SETTINGS_KEY 1
+
+// A structure containing our settings
+typedef struct ClaySettings {
+  GColor BackgroundColor;
+  GColor ForegroundColor;
+  bool SecondTick;
+  bool Animations;
+} __attribute__((__packed__)) ClaySettings;
+
+static void prv_default_settings();
+static void prv_load_settings();
+static void prv_save_settings();
+static void prv_update_display();
+static void prv_inbox_received_handler(DictionaryIterator *iter, void *context);
+static void prv_window_load(Window *window);
+static void prv_window_unload(Window *window);
+static void prv_init(void);
+static void prv_deinit(void);
